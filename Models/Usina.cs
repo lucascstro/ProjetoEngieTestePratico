9935847b2594aa321f82eab6a09fb1153b097995
lucascstro <lukas.castro@live.com>
2021-07-29 01:00:44 -0300

    Implementação da context, string de conexao no webconfig e entidades

---
+++ Models/Usina.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoEngieTestePratico.Models
+{
+    public class Usinas
+    {
+
+        public int Uc { get; set; }
+        public string Fornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
